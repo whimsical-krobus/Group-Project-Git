4620988e55c8c76bcb038f05822e164386c293b2
Linda <lindakjellberg7@gmail.com>
2025-11-12 15:02:30 +0100

    Final commit

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ const createHTML = (movies: Movie[]) => {
 
 
   if (moviesSection) {
-    movieSection.innerHTML = "";
+    moviesSection.innerHTML = "";
   }
 
   movies.forEach((movie) => {
@@ -58,49 +58,30 @@ export const getMovieById = async (id: string) => {
   return await get<Movie>(`${BASE_URL}i=${id}`);
 };
 
-const movies = await getMovies(searchText);
-createHTML(movies);
+export const get = async <T>(url: string) => {
+  const response = await fetch(url);
+  const data: T = await response.json();
 
+  return data;
+};
 
-function createHtml(movies: Movie[]) {
-  const movieSection = document.getElementById("movies");
-
-  if (movieSection) {
-    movieSection.innerHTML = "";
-  }
-
-  movies.forEach((movie) => {
-    const movieContainer = document.createElement("div");
-    const imgContainer = document.createElement("div");
-    const img = document.createElement("img");
-    const title = document.createElement("h2");
-
-    img.src = movie.Poster;
-    img.alt = movie.Title;
-    title.innerHTML = movie.Title;
-
-    movieContainer.addEventListener("click", async () => {
-      const movieDetails = await getMovieById(movie.imdbID);
-      console.log(movieDetails);
-    });
-
-    imgContainer.appendChild(img);
-    movieContainer.appendChild(title);
-    movieContainer.appendChild(imgContainer);
-    moviesSection?.appendChild(movieContainer);
-  });
-}
 
 const form = document.getElementById("form");
 
 form?.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  let searchMovie = "";
-
   const theInput = document.getElementById("searchText");
 
+   let searchText = "";
   if (theInput) {
-    searchMovie = (theInput as HTMLInputElement).value;
+    searchText = (theInput as HTMLInputElement).value;
+  }
+
+  const movies = await getMovies(searchText);
+  createHTML(movies);
+
+  if (theInput) {
+    searchText = (theInput as HTMLInputElement).value;
   }
 });
