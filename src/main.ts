051474b601ff3a899c1acc2b174560ff359198c6
SkagerlingEric <ericskagerling@hotmail.com>
2025-11-12 14:08:54 +0100

    createHtml

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ fetch("https://omdbapi.com/?apikey=416ed51a&s=batman")
   .then((response) => response.json())
   .then((data: OmdbResponse) => {
     console.log(data);
+    
+    
   })
 
 type Movie = {
@@ -18,3 +20,32 @@ type OmdbResponse = {
 
 };
 
+const createHtml = (movies: Movie[]) => {
+  const movieSection = document.getElementById("movies-section");
+
+  if(movieSection) {
+    movieSection.innerHTML = "";
+  }
+  movies.forEach((movie,) => {
+
+    const movieContainer = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const img = document.createElement("img");
+    const title = document.createElement("h1");
+    
+    img.src = movie.Poster;
+    img.alt = movie.Title;
+    title.innerHTML = movie.Title;
+
+    movieContainer.className = "movie";
+    imgContainer.className = "image-container";
+
+    imgContainer.appendChild(img);
+    movieContainer.appendChild(title);
+    movieContainer.appendChild(imgContainer);
+
+    movieSection?.appendChild(movieContainer);
+
+    createHtml(movies);
+  })
+}
